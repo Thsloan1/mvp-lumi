efd9234fbc80d82b317f7fbbd8c4a02c59b3c327
Tena Sloan <tena_sloan@yahoo.com>
2025-09-29 04:02:44 -0700

    Updated FamilyNotesManager.tsx

--- src/components/FamilyNotes/FamilyNotesManager.tsx
+++ src/components/FamilyNotes/FamilyNotesManager.tsx
@@ -72,12 +72,12 @@ export const FamilyNotesManager: React.FC = () => {
                 variant="outline"
                 onClick={() => setCurrentView('lumied-upsell')}
               >
-              >
                 Get Premium Scripts
               </Button>
               <Button
                 onClick={() => setCurrentView('family-script-generator')}
                 icon={MessageCircle}
+              >
                 Generate Custom Script
               </Button>
             </div>
